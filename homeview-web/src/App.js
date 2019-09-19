99eb0ead88e0527da165be5f5ee6ff47fdf9a121
Joe Darling <joe.darling@bt.com>
2019-09-19 10:39:56 +0100

    serverless deployment

--- homeview-web/src/App.js
+++ homeview-web/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      Hello
+      Hello sailor
       <form>
         <input type="textbox" name="Username" className="login-name" />
         <button type="button" className="login-button" name="OK">ok</button>
