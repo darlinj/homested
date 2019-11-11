bd223203ff407678ba2c4e69f6fccd72267b38d3
Joe Darling <joe.darling@bt.com>
2019-11-11 20:43:19 +0000

    reformat

--- homeview-web/src/containers/Home.js
+++ homeview-web/src/containers/Home.js
@@ -1,41 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import { ListGroup } from 'react-bootstrap';
-import { API } from 'aws-amplify';
+import React, {useState, useEffect} from 'react';
+import {ListGroup} from 'react-bootstrap';
+import {API} from 'aws-amplify';
 import './Home.css';
 import Login from './Login';
 
-const Home = (props) => {
-  const [apiResponse, setApiResponse] = useState("Loading...");
+const Home = props => {
+  const [apiResponse, setApiResponse] = useState('Loading...');
 
-	useEffect(() => {
-		if (!props.isAuthenticated) {
-			return;
-		}
-
-    API.get('testApiCall', '/hello').then((response) => {
+  useEffect(() => {
+    if (!props.isAuthenticated) {
+      return;
+    }
+    API.get('testApiCall', '/hello')
+      .then(response => {
         setApiResponse(response.message);
-    }).catch( (e) =>  {
-			console.log(e);
-		});
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  });
 
-	});
+  const renderLander = () => {
+    return <Login {...props} />;
+  };
 
-	const renderLander = () => {
-		return (
-      <Login { ...props }/>
-		);
-	}
-
-	const renderTest = () => {
-		return (
+  const renderTest = () => {
+    return (
       <div className="test">
-				<h1>Test API call</h1>
-				<ListGroup>{apiResponse}</ListGroup>
-			</div>
-		);
-	}
+        <h1>Test API call</h1>
+        <ListGroup>{apiResponse}</ListGroup>
+      </div>
+    );
+  };
 
-		return <div className="Home">{props.isAuthenticated ? renderTest() : renderLander()}</div>;
-}
+  return (
+    <div className="Home">
+      {props.isAuthenticated ? renderTest() : renderLander()}
+    </div>
+  );
+};
 
 export default Home;
