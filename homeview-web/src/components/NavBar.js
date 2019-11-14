34cdf4b31d4481cd8be00f686126d3275489ef35
Joe Darling <joe.darling@bt.com>
2019-11-14 10:18:51 +0000

    branding

--- homeview-web/src/components/NavBar.js
+++ homeview-web/src/components/NavBar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Nav, Navbar} from 'react-bootstrap';
 import UserStatus from './UserStatus';
+import './NavBar.css';
 
 const NavBar = props => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Homeview</Navbar.Brand>
+    <Navbar variant="dark" expand="lg">
+      <Navbar.Brand href="/">
+        <img className="btlogo" src="/bt-logo.png"  alt="" />
+        Homeview
+      </Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav className="mr-auto"></Nav>
