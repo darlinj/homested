31a148936b1af21fcee3c2efbcc4cd5767de1d86
Joe Darling <joe.darling@bt.com>
2019-11-21 14:08:44 +0000

    pan and zoom graph

--- homeview-web/src/containers/HomeNetwork.js
+++ homeview-web/src/containers/HomeNetwork.js
@@ -7,41 +7,42 @@ import {FaCheckCircle, FaMinusCircle, FaTimesCircle} from 'react-icons/fa';
 const HomeNetwork = () => {
   const render = new dagreD3.render();
   var g = new dagreD3.graphlib.Graph()
-    .setGraph({rankdir:"LR"})
+    .setGraph({rankdir: 'LR'})
     .setDefaultEdgeLabel(function() {
       return {};
     });
 
   const addNode = (graph, id, label, icon) => {
-  var html = "<div class=device>";
-      html += '<i class="fa fa-' + icon + '" style="font-size:25px;color:grey;" ></i>';
-      html += "<span class=device-label>"+ label + "</span>";
-      html += "</div>";
-      graph.setNode(id, {
-        labelType: "html",
-        label: html,
-        rx: 5,
-        ry: 5,
-        padding: 0,
-        class: 'type-TOP'
-      });
-  }
+    var html = '<div class=device>';
+    html +=
+      '<i class="fa fa-' + icon + '" style="font-size:25px;color:grey;" ></i>';
+    html += '<span class=device-label>' + label + '</span>';
+    html += '</div>';
+    graph.setNode(id, {
+      labelType: 'html',
+      label: html,
+      rx: 5,
+      ry: 5,
+      padding: 0,
+      class: 'type-TOP',
+    });
+  };
   useEffect(() => {
-    addNode(g, 0, 'BT network', "globe");
-    addNode(g, 1, 'BT home hub', "globe");
-    addNode(g, 2, '2.4GHz Wifi', "wifi");
-    addNode(g, 5, '5 GHz Wifi', "wifi");
-    addNode(g, 13, 'Ethernet', "plug");
-    addNode(g, 3, 'Some Mac book', "laptop");
-    addNode(g, 6, 'Some PC', "laptop");
-    addNode(g, 7, 'Some phone', "mobile");
-    addNode(g, 8, 'Another phone', "mobile");
-    addNode(g, 9, 'Alexa', "microphone");
-    addNode(g, 10, 'Sono hub', "music");
-    addNode(g, 11, 'Wifi extender disk', "wifi");
-    addNode(g, 12, 'Sonos speaker', "music");
-    addNode(g, 4, 'some Android device', "mobile");
-    addNode(g, 14, 'door bell', "bell");
+    addNode(g, 0, 'BT network', 'globe');
+    addNode(g, 1, 'BT home hub', 'globe');
+    addNode(g, 2, '2.4GHz Wifi', 'wifi');
+    addNode(g, 5, '5 GHz Wifi', 'wifi');
+    addNode(g, 13, 'Ethernet', 'plug');
+    addNode(g, 3, 'Some Mac book', 'laptop');
+    addNode(g, 6, 'Some PC', 'laptop');
+    addNode(g, 7, 'Some phone', 'mobile');
+    addNode(g, 8, 'Another phone', 'mobile');
+    addNode(g, 9, 'Alexa', 'microphone');
+    addNode(g, 10, 'Sono hub', 'music');
+    addNode(g, 11, 'Wifi extender disk', 'wifi');
+    addNode(g, 12, 'Sonos speaker', 'music');
+    addNode(g, 4, 'some Android device', 'mobile');
+    addNode(g, 14, 'door bell', 'bell');
 
     g.nodes().forEach(function(v) {
       var node = g.node(v);
@@ -55,7 +56,7 @@ const HomeNetwork = () => {
     g.setEdge(1, 2, {arrowhead: 'noArrow', curve: d3.curveBasis});
     g.setEdge(6, 7, {arrowhead: 'noArrow', curve: d3.curveBasis});
     g.setEdge(5, 6, {arrowhead: 'noArrow', curve: d3.curveBasis});
-    g.setEdge(9, 10,{arrowhead: 'noArrow', curve: d3.curveBasis});
+    g.setEdge(9, 10, {arrowhead: 'noArrow', curve: d3.curveBasis});
     g.setEdge(8, 9, {arrowhead: 'noArrow', curve: d3.curveBasis});
     g.setEdge(11, 12, {arrowhead: 'noArrow', curve: d3.curveBasis});
     g.setEdge(8, 11, {arrowhead: 'noArrow', curve: d3.curveBasis});
@@ -71,8 +72,13 @@ const HomeNetwork = () => {
   const renderGraph = () => {
     var svg = d3.select('.home-network'),
       svgGroup = svg.select('.svgGroup');
-    render.arrows().noArrow = function normal(parent, id, edge, type) {
-    };
+
+    var zoom = d3.zoom().on('zoom', function() {
+      svgGroup.attr('transform', d3.event.transform);
+    });
+    svg.call(zoom);
+
+    render.arrows().noArrow = function normal(parent, id, edge, type) {};
 
     render(d3.select('.home-network g'), g);
     var xCenterOffset = (svg.attr('width') - g.graph().width) / 2;
