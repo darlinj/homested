0dd74f420024b6ad6a2b9eba4a1ff2e55dceca75
Joe Darling <joe.darling@bt.com>
2019-11-21 11:56:18 +0000

    curved lines

--- homeview-web/src/containers/HomeNetwork.js
+++ homeview-web/src/containers/HomeNetwork.js
@@ -5,22 +5,19 @@ import * as d3 from 'd3';
 
 const HomeNetwork = () => {
   const render = new dagreD3.render();
-    var g = new dagreD3.graphlib.Graph()
-      .setGraph({})
-      .setDefaultEdgeLabel(function() {
-        return {};
-      });
+  var g = new dagreD3.graphlib.Graph()
+    .setGraph({})
+    .setDefaultEdgeLabel(function() {
+      return {};
+    });
 
   useEffect(() => {
-
-    // Here we"re setting nodeclass, which is used by our custom drawNodes function
-    // below.
     g.setNode(0, {label: 'BT network', class: 'type-TOP'});
     g.setNode(1, {label: 'BT home hub', class: 'type-S'});
     g.setNode(2, {label: '2.4GHz Wifi', class: 'type-NP'});
-    g.setNode(3, {label: '5 GHz Wifi', class: 'type-DT'});
-    g.setNode(4, {label: 'Ethernet', class: 'type-TK'});
-    g.setNode(5, {label: 'Some Mac book', class: 'type-VP'});
+    g.setNode(5, {label: '5 GHz Wifi', class: 'type-DT'});
+    g.setNode(13, {label: 'Ethernet', class: 'type-TK'});
+    g.setNode(3, {label: 'Some Mac book', class: 'type-VP'});
     g.setNode(6, {label: 'Some PC', class: 'type-VBZ'});
     g.setNode(7, {label: 'Some phone', class: 'type-TK'});
     g.setNode(8, {label: 'Another phone', class: 'type-NP'});
@@ -28,7 +25,7 @@ const HomeNetwork = () => {
     g.setNode(10, {label: 'Sono hub', class: 'type-TK'});
     g.setNode(11, {label: 'Wifi extender disk', class: 'type-NN'});
     g.setNode(12, {label: 'Sonos speaker', class: 'type-TK'});
-    g.setNode(13, {label: 'some Android device', class: 'type-.'});
+    g.setNode(4, {label: 'some Android device', class: 'type-.'});
     g.setNode(14, {label: 'door bell', class: 'type-TK'});
 
     g.nodes().forEach(function(v) {
@@ -38,20 +35,20 @@ const HomeNetwork = () => {
     });
 
     // Set up edges, no special attributes.
-    g.setEdge(3, 4);
-    g.setEdge(2, 3);
-    g.setEdge(1, 2);
-    g.setEdge(6, 7);
-    g.setEdge(5, 6);
-    g.setEdge(9, 10);
-    g.setEdge(8, 9);
-    g.setEdge(11, 12);
-    g.setEdge(8, 11);
-    g.setEdge(5, 8);
-    g.setEdge(1, 5);
-    g.setEdge(13, 14);
-    g.setEdge(1, 13);
-    g.setEdge(0, 1);
+    g.setEdge(3, 4, {arrowhead: 'noArrow', curve: d3.curveBasis});
+    g.setEdge(2, 3, {arrowhead: 'noArrow', curve: d3.curveBasis});
+    g.setEdge(1, 2, {arrowhead: 'noArrow', curve: d3.curveBasis});
+    g.setEdge(6, 7, {arrowhead: 'noArrow', curve: d3.curveBasis});
+    g.setEdge(5, 6, {arrowhead: 'noArrow', curve: d3.curveBasis});
+    g.setEdge(9, 10, {arrowhead: 'undirected'});
+    g.setEdge(8, 9, {arrowhead: 'undirected'});
+    g.setEdge(11, 12, {arrowhead: 'undirected'});
+    g.setEdge(8, 11, {arrowhead: 'undirected'});
+    g.setEdge(5, 8, {arrowhead: 'undirected'});
+    g.setEdge(1, 5, {arrowhead: 'undirected'});
+    g.setEdge(13, 14, {arrowhead: 'undirected'});
+    g.setEdge(1, 13, {arrowhead: 'undirected'});
+    g.setEdge(0, 1, {arrowhead: 'undirected'});
 
     renderGraph();
   });
@@ -59,16 +56,21 @@ const HomeNetwork = () => {
   const renderGraph = () => {
     var svg = d3.select('.home-network'),
       svgGroup = svg.select('.svgGroup');
+    render.arrows().noArrow = function normal(parent, id, edge, type) {
+    };
+
     render(d3.select('.home-network g'), g);
     var xCenterOffset = (svg.attr('width') - g.graph().width) / 2;
     svgGroup.attr('transform', 'translate(' + xCenterOffset + ', 20)');
     svg.attr('height', g.graph().height + 40);
-  }
+  };
 
   return (
     <div>
       <h1>graph</h1>
-      <svg className="home-network" height="500" width="500"><g className="svgGroup" /> </svg>
+      <svg className="home-network" height="100%" width="100%">
+        <g className="svgGroup" />{' '}
+      </svg>
     </div>
   );
 };
